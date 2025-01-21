95b539074cb2a539e79d79df45d234cd7056923c
Olexandr <sashokseven@gmail.com>
2025-01-22 00:57:36 +0200

    add token to persist

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-// import { PersistGate } from "redux-persist/integration/react";
+import { PersistGate } from "redux-persist/integration/react";
 import "./index.css";
 import App from "./components/App";
-import { store } from "./redux/store";
+import { persistor, store } from "./redux/store";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        {/* <PersistGate persistor={persistor}> */}
+        <PersistGate loading={null} persistor={persistor}>
           <App />
-        {/* </PersistGate> */}
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </StrictMode>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,42 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { contactsReducer } from "./contacts/contactsSlice";
 import { filterReducer } from "./filters/filterSlice";
 import { authReducer } from "./auth/authSlice.js";
 
+const persistConfig = {
+  key: "auth",
+  storage,
+  whitelist: ["token"],
+};
+
 const rootReducer = combineReducers({
-  auth: authReducer,
+  auth: persistReducer(persistConfig, authReducer),
   contacts: contactsReducer,
   filter: filterReducer,
 });
 
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
+export const persistor = persistStore(store);
 /**
  * Sixth task
  */
