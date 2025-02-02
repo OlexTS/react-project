c38f2114636b2535337d113034f4516ebd78579f
Olexandr <sashokseven@gmail.com>
2025-02-02 13:31:53 +0200

    add modal to contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,19 +1,36 @@
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { deleteContact } from "../../redux/contacts/contactsOps";
+import Modal from "../Modal/Modal";
 
 
 const Contact = ({ contacts}) => {
+  const [isModalOpen, setIsModalOpen]= useState(false);
+  const [selectedId, setSelectedId] = useState(null)
   const dispatch = useDispatch()
  
+const handleDeleteClick = id =>{
+  setSelectedId(id);
+  setIsModalOpen(true)
+}
+
+const confirmDelete = ()=>{
+  if(selectedId){
+    dispatch(deleteContact(selectedId))
+  }
+  setIsModalOpen(false)
+}
+
   return (
     <>
       {contacts.map(({ id, name, number }) => (
         <li key={id}>
           <p>{name}</p>
           {number}
-          <button type="button" onClick={() => dispatch(deleteContact(id))}>Delete</button>
+          <button type="button" onClick={() => handleDeleteClick(id)}>Delete</button>
         </li>
       ))}
+      <Modal isOpen={isModalOpen} onClose={()=>setIsModalOpen(false)} onConfirm={confirmDelete}/>
     </>
   );
 };
