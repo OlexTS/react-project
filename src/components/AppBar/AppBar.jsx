ea2e02fb721ba23e2afefbf697332d1aa4996f94
Olexandr <sashokseven@gmail.com>
2025-02-10 23:04:58 +0200

    start adding styles with mui

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -2,14 +2,14 @@ import useAuth from "../../hooks/useAuth";
 import AuthNav from "../AuthNav/AuthNav";
 import Navigation from "../Navigation/Navigation";
 import UserMenu from "../UserMenu/UserMenu";
-
+// import AppBar from '@mui/material/AppBar';
 const AppBar = () => {
   const { isLoggedIn } = useAuth();
   return (
-    <div>
+    <>
       <Navigation />
       {isLoggedIn ? <UserMenu /> : <AuthNav />}
-    </div>
+    </>
   );
 };
 
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,13 +1,38 @@
 import css from "./Modal.module.css";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+// import Modal from '@mui/material/Modal';
 
 const Modal = ({ isOpen, onClose, onConfirm }) => {
   if (!isOpen) return null;
   return (
     <div className={css.overlayStyles} onClick={onClose}>
       <div className={css.modalStyles} onClick={(e) => e.stopPropagation()}>
-        <p>Ви впевнені, що хочете видалити контакт?</p>
-        <button onClick={onConfirm}>Так</button>
-        <button onClick={onClose}>Скасувати</button>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 400,
+            bgcolor: "background.paper",
+            border: "2px solid #000",
+            borderRadius: "10px",
+            boxShadow: 24,
+            p: 4,
+            textAlign: "center",
+          }}
+        >
+          <Typography variant="h6" component="h2">
+            Видалення контакту
+          </Typography>
+          <Typography sx={{ mt: 2, marginBottom: "20px" }}>
+            Ви впевнені, що хочете видалити контакт?
+          </Typography>
+          <Button onClick={onConfirm}>Так</Button>
+          <Button onClick={onClose}>Скасувати</Button>
+        </Box>
       </div>
     </div>
   );
